f23295f918ddb07d31fc5566fadf0357de3c18ef
Roy Katzir <roy@teddyvets.com>
2026-01-07 23:33:24 +0000

    File changes

--- src/components/intake-form/PublicIntakeFormFlowFr.jsx
+++ src/components/intake-form/PublicIntakeFormFlowFr.jsx
@@ -263,14 +263,8 @@ export default function PublicIntakeFormFlowFr({ token, formId, initialData = {}
         {/* Content */}
         <div className="relative z-10 container mx-auto px-4 py-4">
           <div className="flex items-center justify-between gap-4">
-            {/* Logo on the left */}
-            <div className="flex-shrink-0">
-              <img
-                src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/687b78971cad562073ed5929/c6a0b9e79_logo_medical.png"
-                alt="Teddy Vets"
-                className="w-20 md:w-28 h-auto object-contain filter brightness-0 invert"
-              />
-            </div>
+            {/* Spacer for balance */}
+            <div className="flex-shrink-0 w-20 md:w-28"></div>
 
             {/* Text in center */}
             <div className="flex-1 text-center text-white">
@@ -283,8 +277,14 @@ export default function PublicIntakeFormFlowFr({ token, formId, initialData = {}
               </p>
             </div>
 
-            {/* Spacer for balance */}
-            <div className="flex-shrink-0 w-20 md:w-28"></div>
+            {/* Logo on the right */}
+            <div className="flex-shrink-0">
+              <img
+                src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/687b78971cad562073ed5929/c6a0b9e79_logo_medical.png"
+                alt="Teddy Vets"
+                className="w-20 md:w-28 h-auto object-contain filter brightness-0 invert"
+              />
+            </div>
           </div>
         </div>
 
