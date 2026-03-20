8748130e7c3404e8cd6b0eec14a31f7a68a80134
Roy Katzir <roy@teddyvets.com>
2026-03-20 15:39:44 +0000

    File changes

--- src/pages/ConsentForms.jsx
+++ src/pages/ConsentForms.jsx
@@ -1,6 +1,6 @@
-
 import React, { useState, useEffect } from "react";
 import { ConsentForm } from "@/entities/all";
+import { Clinic } from "@/entities/Clinic";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
