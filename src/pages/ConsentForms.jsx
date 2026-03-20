64b6de9aca415e756ab40b9c575be217f9f60e67
Roy Katzir <roy@teddyvets.com>
2026-03-20 15:42:23 +0000

    File changes

--- src/pages/ConsentForms.jsx
+++ src/pages/ConsentForms.jsx
@@ -19,10 +19,12 @@ import {
   Filter,
   Copy,
   Loader2,
-  FileCheck, // Added FileCheck icon
-  Shield, // Added Shield icon
-  User // NEW: Added User icon for mobile card
+  FileCheck,
+  Shield,
+  User,
+  Building2
 } from "lucide-react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { format } from "date-fns";
 import { he } from "date-fns/locale";
 import { Link } from "react-router-dom";
